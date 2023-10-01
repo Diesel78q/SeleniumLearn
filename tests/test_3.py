798bb4ec8c976d264ed338ecbb46d51487c77d90
Diesel78q <daniilgoncar7@gmail.com>
2023-10-02 00:45:51 +0200

    f

--- tests/test_3.py
+++ tests/test_3.py
@@ -1,6 +1,6 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains, Keys
-from selenium import webdriver
 import time
 
 def test_login(driver):
@@ -15,15 +15,25 @@ def test_login(driver):
         By.XPATH, '//*[@id="user-name"]')  # Most useful, by XPATH
     username.send_keys(login_standard_user)
     print('Input login')
-    username.send_keys(Keys.BACKSPACE)
-    time.sleep(5)
-    username.send_keys(Keys.BACKSPACE)
-    time.sleep(5)
-    
+    # username.send_keys(Keys.BACKSPACE)
+    # time.sleep(5)
+    # username.send_keys(Keys.BACKSPACE)
+    # time.sleep(5)
+    # username.send_keys('er')
+    # time.sleep(5)
 
-    # password = driver.find_element(By.CSS_SELECTOR, '#password')  # By CSS selector
-    # password.send_keys(password_all)
-    # print('Input password')
+    password = driver.find_element(By.CSS_SELECTOR, '#password')  # By CSS selector
+    password.send_keys(password_all)
+    print('Input password')
+    password.send_keys(Keys.RETURN)
+    
+    filter = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/div/span/select')
+    filter.click()
+    print('Click filter')
+    time.sleep(5)
+    filter.send_keys(Keys.DOWN)
+    time.sleep(5)
+    filter.send_keys(Keys.RETURN)
 
     # button_login = driver.find_element(By.XPATH, '//*[@id="login-button"]')
     # button_login.click()
